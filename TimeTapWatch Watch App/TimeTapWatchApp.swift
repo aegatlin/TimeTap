ede0072c8c54e472304ad1cd2f69eb049a77b929
Austin Gatlin <austin@gatlin.io>
2024-09-29 13:43:53 +1000

    Add watchos app

---
+++ TimeTapWatch Watch App/TimeTapWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeTapWatchApp.swift
+//  TimeTapWatch Watch App
+//
+//  Created by Austin Gatlin on 29/9/2024.
+//
+
+import SwiftUI
+
+@main
+struct TimeTapWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
