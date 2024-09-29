7c5c3da868b17c3f39cb7c7b852ceb7aac4fb267
Austin Gatlin <austin@gatlin.io>
2024-09-29 13:11:10 +1000

    default code for plain ios app

---
+++ TimeTap/TimeTapApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeTapApp.swift
+//  TimeTap
+//
+//  Created by Austin Gatlin on 29/9/2024.
+//
+
+import SwiftUI
+
+@main
+struct TimeTapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
